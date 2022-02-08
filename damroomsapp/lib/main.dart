eaa49407a8595eb20bbb8208c1c91d657e7066e5
Lázaro <lazaroyaesta@gmail.com>
2022-02-08 11:31:44 +0100

    add dependencia de lenguajes
    Se ha añadido una dependencia para cambiar el lenguaje de algunos componentes.

--- damroomsapp/lib/main.dart
+++ damroomsapp/lib/main.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
 import 'package:damroomsapp/pages/reservas_existentes_page.dart';
 
 void main() => runApp(MyApp());
@@ -14,6 +16,15 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'DAMROOMS APP',
       theme: ThemeData.dark(),
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
+      supportedLocales: [
+        const Locale('en', ''), // English, no country code
+        const Locale('es', 'ES'), // Spanish, SPAIN
+      ],
       initialRoute: 'reservas_existentes_page',
       routes: {'reservas_existentes_page': (_) => ReservasExistentesScreen()},
     );
--- damroomsapp/lib/pages/reservas_existentes_page.dart
+++ damroomsapp/lib/pages/reservas_existentes_page.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 
+import 'package:damroomsapp/widgets/background.dart';
+
 class ReservasExistentesScreen extends StatefulWidget {
   @override
   _ReservasExistentesScreenState createState() =>
@@ -13,6 +15,7 @@ class _ReservasExistentesScreenState extends State<ReservasExistentesScreen> {
 
   TextEditingController _inputFieldDateController = new TextEditingController();
 
+  // Lista de widgets de la pagina
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,10 +24,15 @@ class _ReservasExistentesScreenState extends State<ReservasExistentesScreen> {
         ),
         body: ListView(
           padding: EdgeInsets.symmetric(horizontal: 10, vertical: 30),
-          children: [_crearFecha(context)],
+          children: [
+            _crearFecha(context),
+            Divider(),
+            _testResultado()
+          ],
         ));
   }
 
+  // Widget del input fecha
   Widget _crearFecha(BuildContext context) {
     _selectDate(BuildContext context) async {
       DateTime? picked = await showDatePicker(
@@ -46,15 +54,19 @@ class _ReservasExistentesScreenState extends State<ReservasExistentesScreen> {
       enableInteractiveSelection: false,
       controller: _inputFieldDateController,
       decoration: InputDecoration(
-          hintText: 'Fecha de nacimiento',
-          labelText: 'Fecha de nacimiento',
+          hintText: 'Elige fecha',
+          labelText: 'Elige fecha',
           suffixIcon: Icon(Icons.perm_contact_calendar),
           icon: Icon(Icons.calendar_today),
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20))),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
       onTap: () {
         FocusScope.of(context).requestFocus(new FocusNode());
         _selectDate(context);
       },
     );
+  }
+
+  Widget _testResultado() {
+    return ListTile(title: Text('Has elegido: $_fecha'));
   }
 }
